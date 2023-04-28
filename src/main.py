e6a382fe9b4c538d9512560c8a2c651ff35070a4
MMartin09 <mmartin09@outlook.at>
2023-04-28 21:34:00 +0200

    Lint code

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import shutil
 import subprocess
 import sys
 from functools import reduce
-from typing import Optional, Dict, List
+from typing import Dict
 
 import boto3
 
@@ -13,7 +13,7 @@ from src.models.layer_config import LayerConfig
 
 def camel_to_snake_case(val: str) -> str:
     """Convert a given string from Camel Case to Snake Case."""
-    return reduce(lambda x, y: x + ('_' if y.isupper() else '') + y, val).lower()
+    return reduce(lambda x, y: x + ("_" if y.isupper() else "") + y, val).lower()
 
 
 def load_layer_config() -> Dict:
@@ -25,30 +25,35 @@ def load_layer_config() -> Dict:
 
 def install_requirements(requirements_file: str, archive_file: str) -> None:
     archive_file = os.path.join(archive_file, "python")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", requirements_file, "-t", archive_file])
+    subprocess.check_call(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "-r",
+            requirements_file,
+            "-t",
+            archive_file,
+        ]
+    )
 
 
 def compress_archive(archive_name: str, archive_source: str) -> None:
     pathname, _ = os.path.splitext(archive_name)
 
-    shutil.make_archive(
-        base_name=pathname,
-        format="zip",
-        root_dir=archive_source
-    )
+    shutil.make_archive(base_name=pathname, format="zip", root_dir=archive_source)
 
 
 def upload_layer(client, config: LayerConfig, archive_name: str) -> None:
-    layer_content = open(archive_name, 'rb').read()
+    layer_content = open(archive_name, "rb").read()
 
     response = client.publish_layer_version(
         LayerName=config.name,
         Description=config.description,
-        Content={
-            'ZipFile': layer_content
-        },
+        Content={"ZipFile": layer_content},
         CompatibleRuntimes=config.compatible_runtimes,
-        CompatibleArchitectures=config.compatible_architectures
+        CompatibleArchitectures=config.compatible_architectures,
     )
     print(response)
 
@@ -61,9 +66,13 @@ def main():
     for layer in layer_config["layers"]:
         layer_obj = LayerConfig(**layer)
 
-        requirements_file = os.path.join(requirements_directory, layer_obj.requirements_file)
+        requirements_file = os.path.join(
+            requirements_directory, layer_obj.requirements_file
+        )
         requirements_dir = "../example/requirements_dir/"
-        requirements_archive = os.path.join("../example", camel_to_snake_case(layer_obj.name) + ".zip")
+        requirements_archive = os.path.join(
+            "../example", camel_to_snake_case(layer_obj.name) + ".zip"
+        )
 
         install_requirements(requirements_file, requirements_dir)
         compress_archive(requirements_archive, requirements_dir)
